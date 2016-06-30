2b538817132f4c996dbdc1d807f97ff0575e660a
William Knight <kniwilliam@gmail.com>
2016-06-30 00:27:41 -0700

    added styles and validation

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var backwards = '';
-var word = '';
+var backwards = ''.toUpperCase();
+var word = ''.toUpperCase();;
 
 var startXhr2 = function() {
   var $xhr2 = $.getJSON('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20160629T225043Z.cb66c2d8258eab54.4400d700965711edbcfcccc3d1f65154341b935e&lang=en-en&text=' + backwards)
   $xhr2.done(function(data2) {
+    word = word.toUpperCase();
+    backwards = backwards.toUpperCase();
     if (data2.def.length === 0) {
       Materialize.toast(`${word} spelled backwards is ${backwards}. That doesn't make any sense.`, 4000);
     }
@@ -14,7 +16,7 @@ var startXhr2 = function() {
 }
 
 var startXhr = function() {
-  const $input = $('#input').val();
+  var $input = $('#input').val();
   var $xhr = $.getJSON('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20160629T225043Z.cb66c2d8258eab54.4400d700965711edbcfcccc3d1f65154341b935e&lang=en-en&text=' + $input)
   $xhr.done(function(data) {
     if (backwards !== '') {
@@ -22,6 +24,7 @@ var startXhr = function() {
     }
     console.log(data);
     if (data.def[0] === undefined) {
+      $input = $input.toUpperCase();
       Materialize.toast(`${$input} isn't in the dictionary. Try another one.`, 4000);
       return;
     }
@@ -34,5 +37,13 @@ var startXhr = function() {
 };
 
 $('a').click(function() {
+  if ($('#input').val() === '') {
+    Materialize.toast('You have to enter a word.', 4000)
+    return
+  }
   startXhr();
 });
+
+$('#input').click(function() {
+  $('#input').val('');
+})
